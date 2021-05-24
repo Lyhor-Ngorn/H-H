7ba434b2dd731ed06229b7144723cd9b9784bc61
Lyhor-Ngorn <lyhor.ngorn@student.passerellesnumeriques.org>
2021-05-24 08:37:48 +0700

    push for menghour

--- PUBLIC/app.js
+++ PUBLIC/app.js
@@ -1,17 +1,41 @@
-// const PORT = 3000;
-// const IP = "192.168.88.28";
-// const url = "http://"+ IP +":"+PORT;
-let url = "https://hour-hour.herokuapp.com/"
+const PORT = 3000;
+const IP = "192.168.88.14";
+const url = "http://"+ IP +":"+PORT;
+// let url = "https://hour-hour.herokuapp.com/"
 function clickIt(event){
-    event.preventDefault();
-    console.log(text.value)
     let data = {
         "text": text.value
     }
     axios.post(url + "/postData",data).then((response) => {
-        console.log(response.data)
+        localStorage.setItem("text",JSON.stringify(response.data))
+        UL.remove();
+        let  ul = document.createElement("ul");
+        container.appendChild(ul);
+        for(let i of response.data){
+            let li = document.createElement("li");
+            ul.appendChild(li);
+            li.textContent = i.text;
+        }
     });
 }
+
+function loadData(){
+    let getText = localStorage.getItem("text");
+    getText = JSON.parse(getText);
+    let UL = document.querySelector("ul");
+    UL.remove();
+    let  ul = document.createElement("ul");
+    container.appendChild(ul);
+    for(let i of getText){
+            let li = document.createElement("li");
+            ul.appendChild(li);
+            li.textContent = i.text;
+        }
+}
+
+
+
+
 
 // get ul from html
 const UL = document.querySelector("ul");
@@ -19,3 +43,9 @@ const UL = document.querySelector("ul");
 const text = document.querySelector("#text");
 const  sendIt = document.getElementById("submit");
 sendIt.addEventListener("click", clickIt);
+
+const container = document.querySelector(".container");
+
+
+loadData;
+setInterval(loadData,100)
--- server.js
+++ server.js
@@ -6,9 +6,9 @@ app.use(express.urlencoded());
 app.use(express.json());
 
 const PORTS = 3000;
-// const IP = "192.168.88.28";
-// const URL = "http://" + IP + ":" + PORTS;
-let url = "https://hour-hour.herokuapp.com/"
+const IP = "192.168.88.14";
+const URL = "http://" + IP + ":" + PORTS;
+// let url = "https://hour-hour.herokuapp.com/"
 let USER = [
   { name: "lyhor", password: 123456789 },
   { name: "menghour", password: 987654321 },
@@ -21,6 +21,8 @@ app.use(express.static("PUBLIC"));
 
 app.post("/postData", (req, res) => {
   let data = fs.readFileSync("PUBLIC/data.json").toString();
-  console.log(data);
-  res.send(req.body);
+  data = JSON.parse(data);
+  data.push(req.body);
+  fs.writeFileSync("PUBLIC/data.json",JSON.stringify(data));
+  res.send(data);
 });
